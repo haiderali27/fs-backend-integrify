f6df5dcce26e25a2b6684481fc6e07327b20aaf0
unknown <haideraliprofessional@gmail.com>
2023-12-12 12:29:58 +0200

    netlify adjustment changes

--- index.ts
+++ index.ts
@@ -13,11 +13,8 @@ import authRoute from "./routes/authRoute";
 import { checkAuth } from "./middlewares/checkAuth";
 import { responseHandler } from "./middlewares/responsehandler";
 import orderDetailsRoute from "./routes/orderDetailsRoute";
-import router from "./routes/itemsRoute";
 const cors = require('cors');
 const jwt = require("jsonwebtoken");
-const serverless = require("serverless-http")
-
 
 
 const PORT = 8080;
@@ -78,14 +75,9 @@ app.get("/api/v1/protected", checkAuth, (req, res) => {
 app.use(responseHandler);
 app.use(routeNotFound);
 
-
-app.use('/.netlify/functions/api', router)
-
-
 if (process.env.NODE_ENV === "DEV" || process.env.NODE_ENV === "PRODUCTION") {
   app.listen(PORT, () => {
     console.log(`👀 app is running at localhost:${PORT}`);
   });
 }
 export default app;
-module.exports.handler= serverless(app)
